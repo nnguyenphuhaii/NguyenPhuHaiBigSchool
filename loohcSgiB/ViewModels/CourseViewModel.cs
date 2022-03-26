66d05f44e1c761f1f5f3990b07960cdb238e25fd
Nguyễn Hải <nnnnguyenphuhaiiii@gmail.com>
2022-03-27 01:26:11 +0700

    BigSchool

--- loohcSgiB/ViewModels/CourseViewModel.cs
+++ loohcSgiB/ViewModels/CourseViewModel.cs
@@ -1,17 +1,25 @@
-﻿using System;
+﻿using loohcSgiB.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using BigSchoolNguyenPhuHai1911060736.Models;
 
-namespace BigSchoolNguyenPhuHai1911060736.ViewModels
+namespace loohcSgiB.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
+
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime()
         {
