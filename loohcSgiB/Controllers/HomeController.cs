5bf49c84f20fd9724bc2ebfc53495bde49926b44
Nguyễn Hải <nnnnguyenphuhaiiii@gmail.com>
2022-04-02 23:55:25 +0700

    BigSchool

--- loohcSgiB/Controllers/HomeController.cs
+++ loohcSgiB/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using loohcSgiB.ViewModels;
 
 namespace loohcSgiB.Controllers
 {
@@ -18,7 +19,12 @@ namespace loohcSgiB.Controllers
         public ActionResult Index()
         {
             var upcommingCourses = _dbContext.Courses.Include(c => c.Lecturer).Include(c => c.Category).Where(c => c.DateTime > DateTime.Now);
-            return View(upcommingCourses);
+            var viewModel = new CoursesViewModel
+            {
+                UpcomingCourses = upcommingCourses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
         }
 
         public ActionResult About()
---
+++ loohcSgiB/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using loohcSgiB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace loohcSgiB.ViewModels
+{
+    public class CoursesViewModel
+    {
+        public IEnumerable<Course> UpcomingCourses { get; set; }
+        public bool ShowAction { get; set; }
+    }
+}
